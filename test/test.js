2c207e71d27f14f46eb709d4c59c4810615c241f
johnny <ywq19961226>
2019-06-05 10:03:37 +0800

    repair bug.

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ describe('deploy', function() {
     //Options need to be modified before testing
     it('upload success', function(done) {
         deploy.call({
-            public_dir:"E:\\blog\\blog\\public"
+            public_dir:"E:\\blog\\johnny\\public"
         },{
-            accessKey: "9z7tefG6u5kTJu2ROsWFvAXXXXXXXXXXXXXXXXX",
-            secretKey: "tqoc-bdB5K-YVNILQtrgOqXXXXXXXXXXXXXXXXXX",
+            accessKey: "9z7tefG6u5kTJu2ROsWFvACxae5bjxvYxO-Q46Fd",
+            secretKey: "tqoc-bdB5K-YVNILQtrgOq6ASY0bRNRlyry84m8n",
             zone: "Zone_z2",
             scope: "blog",
             dirsToRefresh:"http://blog.icting.cn/"
